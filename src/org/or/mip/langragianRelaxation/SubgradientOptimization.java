9d4b40c7384130a7c8909220295a5a005ff60994
Wang <baohuaw@amazon.com>
2017-06-29 17:46:27 +0800

    More test cases for Subgradient Optimization

--- src/org/or/mip/langragianRelaxation/SubgradientOptimization.java
+++ src/org/or/mip/langragianRelaxation/SubgradientOptimization.java
@@ -11,7 +11,7 @@ import java.util.Map;
 /**
  * Created by baohuaw on 6/29/17.
  */
-public class LangragianRelaxationSolver {
+public class SubgradientOptimization {
     XPRB bcl = new XPRB();
 
     XPRBprob original = bcl.newProb("Original");      /* Create a new problem in BCL */
@@ -20,9 +20,9 @@ public class LangragianRelaxationSolver {
 
     Map<String, XPRBvar> originalVars = new HashMap<>();
     Map<String, XPRBvar> relaxationVars = new HashMap<>();
-    Map<String, XPRBvar> multipliers = new HashMap<>();
+//    Map<String, XPRBvar> multipliers = new HashMap<>();
 
-    double ub = 0;
+//    double ub = 0;
     double currentBest = 0;
 
     double theta = 2;   //param for step size
@@ -32,7 +32,7 @@ public class LangragianRelaxationSolver {
     double t = 1;  //scalar
 
     public static void main(String[] args) {
-        LangragianRelaxationSolver solver = new LangragianRelaxationSolver();
+        SubgradientOptimization solver = new SubgradientOptimization();
         solver.solveOriginalModel();
         solver.subgradientOptimization();
     }
@@ -44,28 +44,28 @@ public class LangragianRelaxationSolver {
         originalVars.put("x4", original.newVar("x4", XPRB.BV, 0, 1));
 
         XPRBexpr originalObj = new XPRBexpr();
-        originalObj.add(originalVars.get("x1").mul(16));
-        originalObj.add(originalVars.get("x2").mul(10));
-//        originalObj.add(originalVars.get("x3").mul(0));
+        originalObj.add(originalVars.get("x1").mul(8));
+        originalObj.add(originalVars.get("x2").mul(9));
+        originalObj.add(originalVars.get("x3").mul(5));
         originalObj.add(originalVars.get("x4").mul(4));
         original.setObj(originalObj);
 
         XPRBexpr cons1 = new XPRBexpr();
-        cons1.add(originalVars.get("x1").mul(8));
-        cons1.add(originalVars.get("x2").mul(2));
-        cons1.add(originalVars.get("x3").mul(1));
-        cons1.add(originalVars.get("x4").mul(4));
-        original.newCtr("Cons 1", cons1.lEql(10));
+        cons1.add(originalVars.get("x1").mul(16));
+        cons1.add(originalVars.get("x2").mul(20));
+        cons1.add(originalVars.get("x3").mul(12));
+        cons1.add(originalVars.get("x4").mul(10));
+        original.newCtr("Cons 1", cons1.lEql(42));
 
-        XPRBexpr cons2 = new XPRBexpr();
-        cons2.add(originalVars.get("x1").mul(1));
-        cons2.add(originalVars.get("x2").mul(1));
-        original.newCtr("Cons 2", cons2.lEql(1));
-
-        XPRBexpr cons3 = new XPRBexpr();
-        cons3.add(originalVars.get("x3").mul(1));
-        cons3.add(originalVars.get("x4").mul(1));
-        original.newCtr("Cons 3", cons3.lEql(1));
+//        XPRBexpr cons2 = new XPRBexpr();
+//        cons2.add(originalVars.get("x1").mul(1));
+//        cons2.add(originalVars.get("x2").mul(1));
+//        original.newCtr("Cons 2", cons2.lEql(1));
+//
+//        XPRBexpr cons3 = new XPRBexpr();
+//        cons3.add(originalVars.get("x3").mul(1));
+//        cons3.add(originalVars.get("x4").mul(1));
+//        original.newCtr("Cons 3", cons3.lEql(1));
 
         original.setSense(XPRB.MAXIM);
 
@@ -87,27 +87,26 @@ public class LangragianRelaxationSolver {
         relaxationVars.put("x4", relaxation.newVar("x4", XPRB.BV, 0, 1));
 
         XPRBexpr relaxObj = new XPRBexpr();
-        relaxObj.add(relaxationVars.get("x1").mul(16 - 8 * lamda));
-        relaxObj.add(relaxationVars.get("x2").mul(10 - 2 * lamda));
-        relaxObj.add(relaxationVars.get("x3").mul(0 - 1 * lamda));
-        relaxObj.add(relaxationVars.get("x4").mul(4 - 4 * lamda));
-//        relaxObj.add(10 * lamda);
+        relaxObj.add(relaxationVars.get("x1").mul(8 - 16 * lamda));
+        relaxObj.add(relaxationVars.get("x2").mul(9 - 20 * lamda));
+        relaxObj.add(relaxationVars.get("x3").mul(5 - 12 * lamda));
+        relaxObj.add(relaxationVars.get("x4").mul(4 - 10 * lamda));
         relaxation.setObj(relaxObj);
         relaxation.setSense(XPRB.MAXIM);
 
-        XPRBexpr cons2 = new XPRBexpr();
-        cons2.add(relaxationVars.get("x1").mul(1));
-        cons2.add(relaxationVars.get("x2").mul(1));
-        relaxation.newCtr("Cons 2", cons2.lEql(1));
-
-        XPRBexpr cons3 = new XPRBexpr();
-        cons3.add(relaxationVars.get("x3").mul(1));
-        cons3.add(relaxationVars.get("x4").mul(1));
-        relaxation.newCtr("Cons 3", cons3.lEql(1));
+//        XPRBexpr cons2 = new XPRBexpr();
+//        cons2.add(relaxationVars.get("x1").mul(1));
+//        cons2.add(relaxationVars.get("x2").mul(1));
+//        relaxation.newCtr("Cons 2", cons2.lEql(1));
+//
+//        XPRBexpr cons3 = new XPRBexpr();
+//        cons3.add(relaxationVars.get("x3").mul(1));
+//        cons3.add(relaxationVars.get("x4").mul(1));
+//        relaxation.newCtr("Cons 3", cons3.lEql(1));
 
         relaxation.mipOptimise();
 
-        double obj = relaxation.getObjVal() + 10 * lamda;
+        double obj = relaxation.getObjVal() + 42 * lamda;
         System.out.println("Relax Obj " + obj);
         System.out.println("x1 = " + originalVars.get("x1").getSol());
         System.out.println("x2 = " + originalVars.get("x2").getSol());
@@ -118,8 +117,8 @@ public class LangragianRelaxationSolver {
         int step = 0;
 
         while (step < 50) {
-            double subgradient = 10 - relaxationVars.get("x1").getSol() * 8 - relaxationVars.get("x2").getSol() * 2 -
-                    relaxationVars.get("x3").getSol() * 1 - relaxationVars.get("x4").getSol() * 4;
+            double subgradient = 42 - relaxationVars.get("x1").getSol() * 16 - relaxationVars.get("x2").getSol() * 20 -
+                    relaxationVars.get("x3").getSol() * 12 - relaxationVars.get("x4").getSol() * 10;
             t = theta * (relaxation.getObjVal() - currentBest) / (subgradient * subgradient);
 
             System.out.println("Step " + step + "  Lamda = " + lamda);
@@ -133,17 +132,15 @@ public class LangragianRelaxationSolver {
             }
 
             relaxObj.reset();
-            relaxObj.add(relaxationVars.get("x1").mul(16 - 8 * lamda));
-            relaxObj.add(relaxationVars.get("x2").mul(10 - 2 * lamda));
-            relaxObj.add(relaxationVars.get("x3").mul(0 - 1 * lamda));
-            relaxObj.add(relaxationVars.get("x4").mul(4 - 4 * lamda));
-//            relaxObj.add(10 * lamda);
+            relaxObj.add(relaxationVars.get("x1").mul(8 - 16 * lamda));
+            relaxObj.add(relaxationVars.get("x2").mul(9 - 20 * lamda));
+            relaxObj.add(relaxationVars.get("x3").mul(5 - 12 * lamda));
+            relaxObj.add(relaxationVars.get("x4").mul(4 - 10 * lamda));
             relaxation.setObj(relaxObj);
-
             relaxation.setSense(XPRB.MAXIM);
             relaxation.mipOptimise();
 
-            obj = relaxation.getObjVal() + 10 * lamda;
+            obj = relaxation.getObjVal() + 42 * lamda;
             System.out.println("Relax Obj " + obj);
             System.out.println("x1 = " + originalVars.get("x1").getSol());
             System.out.println("x2 = " + originalVars.get("x2").getSol());
